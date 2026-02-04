e22b91be53c388aafadce1aae44f19b65d67aadf
Van den Burg, Sven <sven.vandenburg@ns.nl>
2026-02-04 15:51:10 +0100

    Add Azure subscription and tenant ID variables to Terraform configuration

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "azure" {
+  type = object({
+    subscription_id = string
+    tenant_id       = string
+  })
+
+  description = "Provide the azure subscription/tenant where the resources should be placed"
+}
+
 variable "frontdoor_name" {
   description = "The name of the Front Door instance."
   type        = string
