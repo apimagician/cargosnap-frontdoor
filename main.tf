064e4c0574f22e3e93e2a692847c039773bf72a6
Van den Burg, Sven <sven.vandenburg@ns.nl>
2026-02-04 13:47:04 +0100

    Update GitHub Actions workflow to remove unnecessary path and add Azure login secrets documentation; fix resource group name assignment in Terraform configuration

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 module "resource_group" {
   source = "./modules/resource_group"
 
-  name     = var.resource_group_name
+  resource_group_name     = var.resource_group_name
   location = var.location
 }
 
